d3d92d661fd05da8fef1571624e134e5b29291d7
alx00ho <46589739+alx00ho@users.noreply.github.com>
2023-06-19 13:40:28 -0400

    Added More Features
    - More flushed out functions
    - Added randomizer with no repeats
    - Added and implemented Next Country and I Don't Know Buttons
    - Added Play button to start game
    
    - hid Check Answer button and answer box when clicking I Don't Know Button
    
    - Added message to signal that you went through all the countries
    
    * Want to add Score and Timer

--- main.js
+++ main.js
@@ -1,33 +1,84 @@
-const geoURL = "https://restcountries.com/v3.1/all?fields=name";
+//China = 49;
+
+const geoURL = "https://restcountries.com/v3.1/all?fields=name,flags";
 const countryHints = document.getElementById("country-hints");
-const answer = document.getElementById("country-answer");
+const answer = document.getElementById("answer");
 const answerBtn = document.getElementById("check-answer");
+const playBtn = document.getElementById("play-btn");
+const guessContainer = document.getElementById("guess-container");
+const guessContainerItems = document.querySelectorAll(".guess-container-item");
+const initialPage = document.getElementById("initial-page");
+const nextCountryBtn = document.getElementById("next-country-btn");
+const idkBtn = document.getElementById("idk-btn");
 
 answerBtn.addEventListener("click", handleAnswerBtn);
+playBtn.addEventListener("click", handlePlayBtn);
+nextCountryBtn.addEventListener("click", handleNextCountryBtn);
+idkBtn.addEventListener("click", handleIdkBtn);
 
 let numCountries = 0;
 let countryName = "";
+let countriesData = {};
+
+let randIndexArr = [];
+const searched = [];
+let currIndex = 0;
+
+function handlePlayBtn() {
+  initialPage.classList.remove("show");
+  initialPage.classList.add("hide");
+
+  guessContainerItems.forEach((item) => item.classList.remove("hide"));
+  guessContainerItems.forEach((item) => item.classList.add("show"));
+
+  handleCountryName(countriesData);
+}
+
+async function handleNextCountryBtn() {
+  answer.classList.add("show");
+  answerBtn.classList.add("show");
+
+  handleCountryName(countriesData);
+  currIndex++;
+
+  if (currIndex >= numCountries) {
+    countryHints.textContent = "Well Done, that was all of the countries!";
+  }
+
+  countryHints.classList.remove("idk");
+  countryHints.classList.remove("correct");
+}
+
+function handleIdkBtn() {
+  countryHints.classList.add("idk");
+
+  answer.classList.remove("show");
+  answer.classList.add("hide");
+  answerBtn.classList.remove("show");
+  answerBtn.classList.add("hide");
+
+  countryHints.textContent = countryName;
+}
 
 async function countryInfo() {
   try {
     await axios({
       url: geoURL,
       method: "GET",
-    })
-      .then((response) => handleCountryName(response.data))
-      .catch((err) => console.log(err));
+    }).then((response) => {
+      countriesData = response.data;
+      numCountries = countriesData.length;
+      getRandomArray();
+    });
   } catch (e) {
     console.log(e);
   }
 }
 
 const handleCountryName = async function (data) {
-  numCountries = data.length;
+  countryName = data[currIndex].name.common;
 
-  const rand = getRandomInt();
-  // console.log(rand);
-  countryName = data[rand].name.common;
-  // console.log(randomCountry);
+  searched.push(countriesData[currIndex]);
 
   const countryURL = `https://restcountries.com/v3.1/name/${countryName}?fields=capital,continents,flag,currencies,landlocked,population`;
 
@@ -35,32 +86,47 @@ const handleCountryName = async function (data) {
     url: countryURL,
     method: "GET",
   })
-    .then((response) => handleCountryData(response.data))
+    .then((response) => handleCountryData(response.data[0]))
     .catch((err) => console.log(err));
-  // countryHints.textContent += countryName;
 };
 
 const handleCountryData = function (data) {
-  // console.log(data[0].flag);
-  countryHints.textContent += data[0].flag;
+  countryHints.textContent = data.flag;
 };
 
 function handleAnswerBtn() {
-  //   console.log(answer.value);
-  //   console.log(countryName);
-  console.log(answer.value === countryName);
+  console.log(countryName);
+
+  if (
+    countryName.localeCompare(answer.value, undefined, {
+      sensitivity: "accent",
+    }) === 0
+  ) {
+    countryHints.classList.add("correct");
+    countryHints.textContent = "Correct!";
+  }
+  answer.value = "";
 }
 
-const getRandomInt = function () {
-  return Math.floor(Math.random() * numCountries);
-};
+function getRandomArray() {
+  randIndexArr = [...Array(numCountries).keys()];
+  shuffle(randIndexArr);
+}
 
-// const countryNames = async function () {
-//   const res = await fetch(geoURL);
-//   const data = await res.json();
+function shuffle(arr) {
+  let i = arr.length,
+    j = 0,
+    temp;
 
-//   console.log(data);
-// };
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  return arr;
+}
 
 countryInfo();
-// countryNames();
